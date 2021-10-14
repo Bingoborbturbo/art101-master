f5204b21e4d46150c9fc69812e54266f774275f8
Bingoborbturbo <61530115+Bingoborbturbo@users.noreply.github.com>
2021-10-13 20:56:47 -0700

    lab 4 submission

---
+++ lab4/js/lab.js
@@ -0,0 +1,11 @@
+//Summary. This is a calculator created in Javascript using HTML and CSS.
+//Conner Murphy Jonathan Palafox
+//10/13/2021
+//const inputField = document.getElementById('input'); ( Value input variable)
+
+//Create the basic structure for the calculator and buttons with html
+   //Use the css file to determine height, width, etc of the calculator
+      //Use the css file to format a display to view calculations
+     //Add required buttons
+  //Format the buttons in css
+ //Use Javascript to activate the calculator
