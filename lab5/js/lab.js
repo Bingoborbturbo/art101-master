16f4379692212c5583690f2e35ecfc6180262b64
Bingoborbturbo <61530115+Bingoborbturbo@users.noreply.github.com>
2021-10-19 02:38:52 -0700

    lab5 completion

---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+//Author: Jonathan Palafox
+//created: October 18th 2021
+//license: Public Domain
+//
+
+// Define Variables
+make = "Nissan";
+model = "180sx";
+color = "purple"
+year = "1992";
+
+//calculate
+age = 2021 - year; 
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
